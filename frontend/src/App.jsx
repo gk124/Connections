d2f6df152183858b0169ada9244e93225532975e
gk124 <gunjanku109@gmail.com>
2024-03-18 13:42:27 +0530

    silly change 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,25 +3,32 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
-
 function App() {
-  const {authUser}=useAuthContext();
+  const { authUser } = useAuthContext();
   return (
-    <div className="flex flex-col p-2 max-h-[75vh] items-center justify-center overflow-auto relative">
-    <h1 className="text-5xl lg:font-semibold md:font-bold sm:font-normal text-black italic">
+    <div className="app flex flex-col p-2 h-screen items-center justify-center overflow-auto relative">
+      <h1 className="text-5xl lg:font-semibold md:font-bold sm:font-normal text-black italic">
         CONNECTIONS
-    </h1>
-    <Routes>
-        <Route path='/' element={authUser ? <Home/> : <Navigate to={"/login"} />} />
-        <Route path='/login' element={authUser ? <Navigate to="/" /> : <Login/>} />
-        <Route path='/signup' element={authUser ? <Navigate to="/" /> : <SignUp/>} />
-    </Routes>
-    <Toaster/>
-</div>
-
+      </h1>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
